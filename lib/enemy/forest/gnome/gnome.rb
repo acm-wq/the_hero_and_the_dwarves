63cfb402c870cfb3b74771d790374e55e009ab26
acm-wq <camisglhdadae@gmail.com>
2024-09-20 11:43:14 +0000

    update base and enemy code

--- lib/enemy/forest/gnome/gnome.rb
+++ lib/enemy/forest/gnome/gnome.rb
@@ -1,9 +1,7 @@
-require_relative '../../../game/base_entity'
-
 module Game
   class Gnome < BaseEntity
-    attr_accessor :target  
-    attr_reader :image     
+    attr_accessor :target
+    attr_reader :image
 
     def initialize(x = 0, y = 0, speed = 1, health = 50, damage_player = 1)
       super(x, y, speed, health, damage_player)
@@ -11,57 +9,16 @@ module Game
       @image = Gosu::Image.new('lib/enemy/forest/gnome/sprite/gnome.png')
     end
 
-    # ...
     def set_target(target)
       @target = target
     end
 
-    # ...
     def update
-      return unless @target # ...
+      return unless @target
 
-      # ...
-      target_x = @target.x
-      target_y = @target.y
-
-      # ...
-      dx = target_x - @x
-      dy = target_y - @y
-
-      # ...
-      distance = Math.sqrt(dx**2 + dy**2)
-      return if distance == 0  # ...
-
-      # ...
-      if collides_with?(@target, 30)
-        deal_damage_to_player
-      else
-        # ...
-        step_x = (dx / distance) * @speed
-        step_y = (dy / distance) * @speed
-
-        # ...
-        move(step_x, step_y)
-      end
+      attack(@target)  # Use the attack method from BaseEntity
     end
 
-    # ...
-    def deal_damage_to_player
-      if @target.health > 0
-        if @target.defending_animation  # ...
-          reduced_damage = @damage_player / 2  
-          @target.health -= reduced_damage
-          puts "The gnome attacks, but the player defends! Damage reduced to #{reduced_damage}. I've got my health: #{@target.health}"
-        else
-          @target.health -= @damage_player
-          puts "A gnome is attacking a player! The player has health left: #{@target.health}"
-        end
-      else
-        puts "The player is defeated!"
-      end
-    end
-
-    # ...
     def draw
       @image.draw(@x, @y, 1)
     end
--- lib/game/base_entity.rb
+++ lib/game/base_entity.rb
@@ -1,25 +1,25 @@
 module Game
   class BaseEntity
     attr_accessor :x, :y, :speed, :health, :strength, :dexterity, :intelligence, :constitution, :charisma, :resistance,
-                  :luck, :race
+                  :luck, :race, :damage_player
 
     RACES = %w[Unknown Human Elf Dwarf Orc]
 
     def initialize(x = 0, y = 0, speed = 2, health = 100, damage_player = 0, strength = 0, dexterity = 0, intelligence = 0, constitution = 0,
                    charisma = 0, resistance = 0, luck = 0, race = 'Unknown')
-      @x = x                          # X position of the entity
-      @y = y                          # Y position of the entity
-      @speed = speed                  # Speed of the entity
-      @health = health                # Health of the entity
-      @damage_player = damage_player  # Damage to Player (enemy)
-      @strength = strength            # Strength of the entity
-      @dexterity = dexterity          # Dexterity of the entity
-      @intelligence = intelligence    #
-      @constitution = constitution    #
-      @charisma = charisma            #
-      @resistance = resistance        #
-      @luck = luck                    #
-      @race = race                    #
+      @x = x
+      @y = y
+      @speed = speed
+      @health = health
+      @damage_player = damage_player
+      @strength = strength
+      @dexterity = dexterity
+      @intelligence = intelligence
+      @constitution = constitution
+      @charisma = charisma
+      @resistance = resistance
+      @luck = luck
+      @race = race
     end
 
     def move(dx, dy)
@@ -32,13 +32,50 @@ module Game
       # This method should be overridden in subclasses
     end
 
-    # ...
     def distance_to(other_entity)
       Math.sqrt((@x - other_entity.x)**2 + (@y - other_entity.y)**2)
     end
 
     def collides_with?(other_entity, collision_distance = 30)
       distance_to(other_entity) < collision_distance
+    end
+
+    # New methods for attack logic
+    def attack(target, collision_distance = 30)
+      return unless target.health > 0
+
+      if collides_with?(target, collision_distance)
+        deal_damage_to(target)
+      else
+        move_towards(target)
+      end
+    end
+
+    private
+
+    def deal_damage_to(target)
+      if target.defending_animation
+        reduced_damage = @damage_player / 2
+        target.health -= reduced_damage
+        puts "The attack was defended! Damage reduced to #{reduced_damage}. Target's health: #{target.health}"
+      else
+        target.health -= @damage_player
+        puts "Attacked successfully! Target's health left: #{target.health}"
+      end
+
+      puts "The target is defeated!" if target.health <= 0
+    end
+
+    def move_towards(target)
+      dx = target.x - @x
+      dy = target.y - @y
+      distance = Math.sqrt(dx**2 + dy**2)
+      return if distance == 0
+
+      step_x = (dx / distance) * @speed
+      step_y = (dy / distance) * @speed
+
+      move(step_x, step_y)
     end
   end
 end
